6c1d528a0c4b9db451e17228ae0e8dc6a5d39c75
speschellk <kayc@stanford.edu>
2016-03-22 14:16:37 -0700

    Update rps-game.js

--- rps-game.js
+++ rps-game.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
 
+  // resets html and css to new game settings
   function newGame(){
     $("#pieces-played").html("");  
     $("#game-results").html("");
     $("#new-game").css("display", "none");
   }
 
+  // compares player and computer choice; returns winner
   function winner(playerChoice, computerChoice) {
     if (playerChoice === computerChoice)
       return "Draw";
-
+    
     if (playerChoice === "Paper") {
       if (computerChoice === "Rock") {
         return "You win";
@@ -31,16 +33,26 @@ $(document).ready(function() {
     }
   }
 
+  // rock, paper, scissors event listeners
   $(".btn-primary").click(function() {
-    var playerChoice = $(this).text();
+    var playerChoice = $(this).attr("id");
+
+    // API call to heroku app
     $.get("http://rock-paper-scissors-api.herokuapp.com/", function(data) {
       var computerChoice = data;
+
+      // states which choices were played
       $("#pieces-played").html("You played " + playerChoice + ", I played " + computerChoice);
+
+      // states winner
       $("#game-results").html(winner(playerChoice, computerChoice));
     });
+
+    // shows "Play Again" button
     $("#new-game").show();
   });
 
+  // play new game event listener to start new game
   $(".btn-default").click(function() {
     newGame();
   });
